80c9304443159cc1a216543e02d03b5cf30488c0
dwwang <wangdawei@lidig.com>
2021-03-30 14:18:41 +0800

    feat: 使用hooks写法

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,55 +1,37 @@
-import React, { ComponentClass, PureComponent } from 'react';
+import React from 'react';
 import { View, Text } from '@tarojs/components';
 import { AtButton } from 'taro-ui';
 import { WholeState } from '@/types/globals';
-import { DispatchProp } from '@/types/dva';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import './index.scss';
 
-const mapStateToProps = ({ app, loading }: WholeState) => ({
-  login: app.login,
-  loading: loading.effects['app/login'],
-});
-
-type StateProps = ReturnType<typeof mapStateToProps>;
-type OwnProps = {};
-type Props = DispatchProp & StateProps & OwnProps;
-
-type State = {};
-
-interface Index {
-  props: Props;
-}
-
-@connect(mapStateToProps)
-class Index extends PureComponent<Props, State> {
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return (
-      <View className="index">
-        <AtButton className="add_btn" onClick={this.onClick}>
-          +
-        </AtButton>
-        {/* <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button> */}
-        <View>
-          <Text>{1}</Text>
-        </View>
-        <View>
-          <Text>Hello, World</Text>
-        </View>
-      </View>
-    );
-  }
-
-  onClick = () => {
-    this.props.dispatch({
-      type: 'app/login',
+const Home: React.FC<{}> = props => {
+  console.log('props: ', props);
+  const dispatch = useDispatch();
+  const [num, setNum] = React.useState<number>(0);
+  const app = useSelector((state: WholeState) => state.app);
+  const handleClick = () => {
+    dispatch({
+      type: 'app/updateState',
+      payload: { login: !app.login }
     });
   };
-}
 
-export default Index as ComponentClass<OwnProps, State>;
+  return (
+    <View className="index">
+      <AtButton className="add_btn" onClick={handleClick}>
+        +
+      </AtButton>
+      {/* <Button className='dec_btn' onClick={this.props.dec}>-</Button>
+      <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button> */}
+      <View>
+        <Text>{num}</Text>
+      </View>
+      <View>
+        <Text>Hello, World</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
