4d52a93b2121157336fe8d45cb4a532007ed16d9
dwwang <wangdawei@lidig.com>
2021-03-31 11:36:42 +0800

    feat: 集成ahooks

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,27 +3,25 @@ import { View, Text } from '@tarojs/components';
 import { AtButton } from 'taro-ui';
 import { WholeState } from '@/types/globals';
 import { useSelector, useDispatch } from 'react-redux';
+import { usePersistFn } from 'ahooks';
 import './index.scss';
 
 const Home: React.FC<{}> = props => {
-  console.log('props: ', props);
   const dispatch = useDispatch();
   const [num, setNum] = React.useState<number>(0);
   const app = useSelector((state: WholeState) => state.app);
-  const handleClick = () => {
+  const handleClick = usePersistFn(() => {
     dispatch({
       type: 'app/updateState',
       payload: { login: !app.login }
     });
-  };
+  });
 
   return (
     <View className="index">
       <AtButton className="add_btn" onClick={handleClick}>
         +
       </AtButton>
-      {/* <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-      <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button> */}
       <View>
         <Text>{num}</Text>
       </View>
